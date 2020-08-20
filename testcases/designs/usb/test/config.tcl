72b6ccc93b29ba45db455549260e4c96569d0c1b
agorararmard <aagouhar@efabless.com>
2020-08-20 22:34:19 +0200

    added 3 new testcases

---
+++ testcases/designs/usb/test/config.tcl
@@ -0,0 +1,4 @@
+# General config
+set ::env(DESIGN_NAME) $::env(DESIGN)
+set ::env(SPICE_NETLIST) $::env(test_dir)/usb.spice
+set ::env(CURRENT_NETLIST) $::env(test_dir)/usb.lvs.powered.v
---
+++ testcases/designs/usb_cdc_core/test/config.tcl
@@ -0,0 +1,4 @@
+# General config
+set ::env(DESIGN_NAME) $::env(DESIGN)
+set ::env(SPICE_NETLIST) $::env(test_dir)/usb_cdc_core.spice
+set ::env(CURRENT_NETLIST) $::env(test_dir)/usb_cdc_core.lvs.powered.v
